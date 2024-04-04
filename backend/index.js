4670037dfa985c029de513daba68111cfc1f2056
Ravi <ravigargrud@gmail.com>
2024-04-05 00:01:59 +0530

    initial setup and linking done

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,24 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(3000, () => {
+app.get('/api/sample', (req, res) => {
+    myobj = [
+        {
+        name: 'John',
+        age: 30,
+        city: 'New York'
+        },
+        {name: 'Peter',
+        age: 45,
+        city: 'New York'},
+    ];
+    res.send(myobj);
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const[data, setData] = useState([]);
+
+useEffect(() => {
+  axios.get('/api/sample')
+  .then((response) => {
+    setData(response.data);
+  })
+  .catch((error) => {
+    console.error('There was an error!', error);
+  })
+});
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      Hello World!
+      {data.map((item, index) => (
+        <div key={index}>
+          <h1>{item.name}</h1>
+          <h2>{item.age}</h2>
+          <h2>{item.city}</h2>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
